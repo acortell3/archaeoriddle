e1e6e0dc0b4cc1832c9ac7e35fbae9dc6dc00eaa
Simon <simon@elisya.org>
2023-08-21 10:26:52 +0100

    minor estethics changes to program

--- doc/shinyapp/app.R
+++ doc/shinyapp/app.R
@@ -224,7 +224,8 @@ ui <- fluidPage(
 							   tags$br(), img(src='eaa_logo.jpg'), tags$br(), tags$br(), img(src='ba_logo.png',width="150px")
 							   ),
                         column(width = 5,
-                               strong(tags$h2(style='font-weight:900;color:white;font-family:Courier New'," The Archaeoriddle Workshop, Saturday the 2nd of September 2023, Lanyon Building, room G059")),                      
+                               strong(tags$h2(style='font-weight:900;color:white;font-family:Courier New'," The Archaeoriddle Workshop"),
+									  tags$h3(style='font-family:Courier New',"Saturday 2nd September 2023, Lanyon Building, room G059")),                      
 							   tags$br(),
                                helpText(HTML("<p style='font-family:Courier New;'> The Archaeoriddle Workshop, where final results and proposal from participant will be presented wil be held at <a href='https://www.e-a-a.org/EAA2023/'>EAA Belfast</a> on the Saturday Afternoon at Lanyon Building (G059) starting at 14h00  </p> </br>")),                      
                                helpText(HTML("<p style='font-family:Courier New;'> <b> Program:</b> </p>")),                      
