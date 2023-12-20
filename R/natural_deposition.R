e21b23c99571dc1227c84643174151486519ef56
ismaRP <isma.kosspub@gmail.com>
2023-12-20 14:43:28 +0000

    Create package with functions and show formated code along with documentation in 1st chapter

---
+++ R/natural_deposition.R
@@ -0,0 +1,120 @@
+## Function 6. Depth protocol deposition
+#' @title Depth protocol deposition
+#' @description
+#' It distributes the samples produced in one specific year along the depth of the
+#' site, without any kind of post-depositional alteration, and according to 
+#' pre-established post-deposition rates. Returns a vector with the samples exponentially
+#' distributed. The vector is as long as L/r and the error (prop_buried, \eqn{\theta_{\epsilon}}) is considered.
+#' @param W_t Integer (user provided), vector or data.frame. It is the number samples
+#' produced at a specific 't'.
+#' @param r Is the natural deposition rates. At this moment. Values greater than 0.5 are not accepted.
+#' If values with two or more decimals are provided, the function will automatically round 
+#' the value to one decimal.
+#' @param max_bone_thickness Maximum thickness of bones within the assemblage. Four
+#' values are possible: `'s'` (small) = 2.5 cm; `'m'` (medium) = 5 cm; `'l'` (large) = 10 cm
+#' and `'vl'` (very large) = 20 cm. Default is `'m'`.
+#' @param prop_buried Proportion of samples buried sample at tmax, considering error. Pb 
+#' needs to be smaller than 1. Default is 0.9999, which stands for 99.99%.
+#' This is \eqn{\theta_{\epsilon}} in equation 9
+#' @return A vector of samples buried at times from tl to tm
+#' @export
+D_along <- function(W_t, r, max_bone_thickness = c("m", 's', 'l', 'vl'),
+                    prop_buried = .9999){
+  
+  # Define parameter r
+  if(r > 0.5) stop("values > 0.5 are not accepted for param 'r'")
+  r <- round(r, 1)
+  # Constraints for parameter Pb
+  if (prop_buried >= 1) stop("Pb must be lower than 1")
+  
+  # Define parameter Max_bone_thickness (L)
+  max_bone_thickness = match.arg(max_bone_thickness)
+  if (max_bone_thickness == 's'){
+    L <- 2.5
+  } else if (max_bone_thickness == 'm'){
+    L <- 5
+  } else if (max_bone_thickness == 'l'){
+    L <- 10
+  } else if (max_bone_thickness == 'vl'){
+    L <- 20
+  }
+  
+  # Define tmax
+  tm <- L/r
+  # Estimate lambda
+  l <- -log(1 - prop_buried) / tm
+  
+  ss <- rep(0, round(tm)) ## Vector to distribute samples over
+  tl <- 0 # Year where the sample is deposited
+  tu <- 1 # Year when it is covered
+  
+  for (i in 1:tm){
+    Wb <- W_t * (1 - exp(-l*(tu-tl))) # Apply formula for tu
+    Wbprev <- W_t * (1 - exp(-l*((tu-1)-tl))) # Calculate for previous to tu
+    ss[i] <- round(Wb - Wbprev) # Number of samples for each year
+    tu <- tu + 1
+  }
+  
+  return(ss)
+}
+
+## 7. Archaeological deposition record
+#' @title Archaeological deposition record
+#' @description
+#' Generate archaeological deposition record over time and depth
+#' It iterates over D_along to spread the amount of samples produced at each time point over different profundities
+#' @param x Vector with the number of samples per year. As produced as produced by iterating over A_rates.
+#' @param area Only if persqm = TRUE. In this case, the total area of the site 
+#' must be provided
+#' @param ts Time-span, the number of years considered for the process
+#' @param InitBP Initial year considered for the process. In BP.
+#' @param persqm If TRUE, the total record is divided by the area of the site
+#' (in square meters), so that the output is per square meter. Default is FALSE
+#' @param ... This function uses the functions `D_along()`. The additional 
+#' arguments can be added.
+#' @return A square matrix of size ts. It contains the amount of samples deposited at each year (columns) and each depth (rows)
+#' @export
+Rec_c <- function(x, area, ts, InitBP, persqm = FALSE,...){
+  
+  ## Whether sqm division must be included or not
+  if (persqm == TRUE){
+    x <- x / area
+  }
+  
+  ## Spread dates along different depths
+  matdim <- length(x)
+  mat <- matrix(nrow=matdim, ncol=matdim)
+  
+  for (i in 1:matdim){
+    new <- D_along(x[i], ...)
+    st <- i - 1
+    pos <- c(rep(0, st), new)
+    pos <- pos[1:matdim]
+    mat[, i] <- pos
+  }
+  mat[is.na(mat)] <- 0
+  
+  ## Names for columns (each year)
+  years <- seq(InitBP, InitBP-ts)
+  nyears <- c()
+  for (i in 1:matdim){
+    nyears[i] <- paste0(years[i], " BP")
+  }
+  colnames(mat) <- nyears
+  
+  ## Names for rows (each depth)
+  # Extract arguments as a list
+  extract_args <- function(x, ...){ 
+    extras <- list(...)
+    return(list(extras=extras)) 
+  }
+  
+  dr <- extract_args(D_along, ...)
+  dr <- dr$extras$r
+  
+  d <- rev(cumsum(rep(dr, nrow(mat)))) ## computes depths
+  rownames(mat) <- paste0("d = ", d, " cm")
+  
+  return(mat)
+}
+
---
+++ R/population.R
@@ -0,0 +1,148 @@
+## Function 1. Generation of population dynamics
+#' @title Generation of population dynamics
+#' @description
+#' Protocol to generate a stochastic demographic process
+#' @param x Input data with initial population matrix. A data frame or matrix
+#' with two columns and nrow = Initial population. One row per individual.
+#' The first column is the age of the individual.
+#' The second column is the sex of the individual, and must be c("F","M").
+#' The columns must be named Age and Sex respectively.
+#' @param K Carrying capacity.
+#' @param W_fert_age Vector with two values. The first value is the youngest age
+#' at which is considered that women can have children for prehistoric societies.
+#'  The second value is the oldest age at which is considered that women can 
+#'  have children. Default is c(10,30).
+#' @param M_fert_age Vector with two values. The first value is the youngest age
+#' at which is considered that men can have children for prehistoric societies.
+#' The second value is the oldest age at which is considered that men can have 
+#' children. Default is c(15,40)
+#' @param p_offspring: Probability of a woman having a son per year. Default is 0.3.
+#' @param prob Probability that an individual will die per year if total population
+#' exceeds K. Default is 0.8
+#' @param ... Arguments passed to [death()]. The mortality probability by age matrix. 
+#' Their arguments can be added.
+#' @return data.frame with two columns, where the number of rows is the number of
+#' people. The first column contains the ages and the second column contains the sex.
+#' @export
+#'
+Gpd <- function(x, K, W_fert_age = c(10, 45), M_fert_age = c(15, 55),
+                p_offspring = 0.3, prob = 0.8, ...){
+  
+  ### Process of having offspring
+  
+  # Female fertile population
+  W <- x[x[,2]=="F", ]
+  W_fert <- W[W$Age > W_fert_age[1] & W$Age < W_fert_age[2], ]
+  # Male fertile population
+  M <- x[x[,2]=="M", ]
+  M_fert <- M[M$Age > M_fert_age[1] & M$Age < M_fert_age[2], ]
+  
+  ## Probability of having descendance per woman
+  # Penalisation in case there are too few men
+  pen <- round(nrow(M_fert)*2 / nrow(W_fert), 2) ## Assumes one man can have two women
+  pen[pen>1] <- 1 ## The men penalisation can never multiply the birth rate per woman
+  
+  ## Probability of a woman having a son per year
+  p_offspring <- p_offspring*pen
+  
+  # Aging process. They get one year older
+  x$Age <- x$Age+1
+  
+  ### Births are new population that's added
+  n_offspring <- sum(rbinom(nrow(W_fert), 1, p_offspring))
+  new_pop <- data.frame(
+    "Age" = rep(0, n_offspring),
+    "Sex" = sample(c("M","F"), n_offspring, prob=c(0.5, 0.5), replace=TRUE))
+  x <- rbind(x, new_pop)
+  
+  ### Process of dying
+  vec_d <- apply(x, 1, death, ...)
+  x <- x[vec_d==0, ]
+  
+  ## Apply carrying capacty restrictions
+  x <- K_lim(x, K = K, prob = prob)
+  
+  return(x)
+}
+
+
+## Function 2. Simulation of death process
+#' @title Simulation of death process
+#' @description
+#' For a single individual, returns whether it lives (0) or dies (1)
+#' Meant to be used with apply on the population data.frame x
+#' @param x A vector or data.frame with a single row from the population matrix.
+#' It must contain two values or columns, Age and Sex
+#' @param pd The probability matrix for mortality by age
+#' The age-structured data frame is based on Gurven, Kaplan and Supa, 2007.
+#' It is extracted adapted after computation from their text (not graphs or tables)
+#' @return A value (0,1) where 0 = person lives and 1 = person dies, based
+#' on pd (the probability matrix)
+#' @export
+death <- function(x, pd=data.frame("Age" = c(0:99),
+                                   "P_d" = c(rep(0.14,1),
+                                             rep(0.16,4),
+                                             rep(0.05,5),
+                                             rep(0.01,24),
+                                             rep(0.03,14),
+                                             rep(0.1,10),
+                                             rep(0.3,42)))){
+  age <- as.numeric(x[1])
+  return(rbinom(1, 1, prob=pd[pd$Age==age, 2]))
+}
+
+## Function 3. Simulation of carrying capacity limitation
+#' @title Simulation of carrying capacity limitation
+#' @description
+#' If the population exceeds the carrying capacity, it eliminates oversize
+#' with prob probability per person exceeding.
+#' @param x Data frame or matrix. Population (number of people)
+#' @param K Integer. Carrying capacity. Provided by the user
+#' @param prob It is the probability of dying when surpassing carrying capacity
+#' @return Returns a data.frame with the updated population
+#' @export
+K_lim <- function(x, K, prob=0.8){
+  p <- nrow(x)
+  if (p>K){
+    o <- rbinom(p-K, 1, prob)
+    o <- sum(o[o==1])
+    o[o==0] <- 1 ## Avoids problem eliminating all the df if remove == 0
+    x <- x[-sample(1:nrow(x), o, replace=FALSE), ]
+  }
+  return(x)
+}
+
+
+## Function 4. Stochastic population generation
+#' @title Stochastic population generation
+#' @description
+#' It reproduces the population stochastic process. The result is a vector with the 
+#' number of individuals for each year.
+#' @param pop_size Integer, the initial population
+#' @param K Only if model_pop = TRUE. In this case, it is the carrying capacity
+#' @param ts Time-span, the number of years considered for the process
+#' @param prob Probability that an individual will die if total population
+#' exceeds K. Default is 0.8
+#' @param ... Additional arguments passed to [Gpd()], and in turn to [death()]
+#' @return A vector with the population size for each year from 1 to ts
+#' @export
+Pop_stoch <- function(pop_size, K, ts, prob = 0.8, ...){
+  
+  ## Create initial population
+  pop_matrix <- data.frame(
+    "Age" = sample(10:30, pop_size, 10:30, replace = TRUE),
+    "Sex" = sample(c("M","F"), pop_size, prob = c(0.5,0.5), replace = TRUE)
+  )
+  
+  # Initialize vector with population size for each year  
+  pop <- vector(length=ts)
+  pop[0] <- nrow(pop_matrix)
+  ## Run stochastic process  
+  for (i in 1:ts){
+    pop_matrix <- Gpd(pop_matrix, K=K, prob=prob, ...)
+    pop[i] <- nrow(pop_matrix)
+  }
+  
+  return(pop)
+}
+
